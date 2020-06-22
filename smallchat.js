086bb806826ec0509bbb640024a4bf826a65ea9d
yangsalt <kibkibe@naver.com>
2020-06-22 12:10:24 +0900

    Update smallchat.js

--- smallchat.js
+++ smallchat.js
@@ -14,7 +14,7 @@
 
 	1. 세션방의 대문에 해당하는 페이지에서 [설정]->[API 스크립트]를 선택해 스크립트 수정 페이지로 들어갑니다. (PRO 계정에서만 이 메뉴가 보입니다.)
 	2. New Script에 이 코드들을 복사해 붙여놓습니다.
-	3. [옵션] 27번째 줄과 29번째 줄 중 원하는 형식을 선택해서 해당 줄의 주석표시 (맨 앞의 //)를 지워줍니다.
+	3. [옵션] 26번째 줄의 if (true) 부분을 수정하여 잡담을 플레이어로 할지 캐릭터로 할지를 변경할 수 있습니다.
 	4. [Save Script]로 저장합니다. 
 	5. 페이지 아래쪽의 API Output Console에 에러 메시지가 표시되지 않는다면 정상적으로 적용된 것입니다. 세션방에서 테스트를 진행할 수 있습니다.
 	6. 채팅창에 '! 하고싶은말'의 형식으로 입력해 테스트를 해봅니다.
@@ -23,10 +23,11 @@ on("chat:message", function(msg)
 {
 if (msg.type == "api"){
     if (msg.content.indexOf("! ") === 0) {
-	//이 아랫줄의 코드에서 맨 앞의 //를 지우면 플레이어 As로 잡담을 합니다.
-	//sendChat("player|"+msg.playerid,"<span style='color:#aaaaaa'>"+msg.content.substring(2, msg.content.length)+"</span>");
-	//이 아랫줄의 코드에서 맨 앞의 //를 지우면 PC로 As를 유지하고 잡담을 합니다.
-	//sendChat(msg.who,"<span style='color:#aaaaaa'>"+msg.content.substring(2, msg.content.length)+"</span>");
+        if (true) { //괄호 안에 true를 입력하시면 플레이어 As로, false를 입력하시면 선택되어 있는 As를 유지한 채 잡담을 합니다.
+            sendChat("player|"+msg.playerid,"<span style='color:#aaaaaa'>"+msg.content.substring(2, msg.content.length)+"</span>");
+        } else {
+            sendChat(msg.who,"<span style='color:#aaaaaa'>"+msg.content.substring(2, msg.content.length)+"</span>");
+        }
     }
 }
 });
