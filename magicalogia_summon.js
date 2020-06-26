55e2da27b35f129307a02f5afbc1b23025902e58
yangsalt <kibkibe@naver.com>
2020-06-26 14:50:25 +0900

    Update magicalogia_summon.js

--- magicalogia_summon.js
+++ magicalogia_summon.js
@@ -9,13 +9,15 @@
 
 	[ 사용법 ]
 	1. roll20 세션방에서 원형을 사용할 페이지에서 GM레이어를 선택합니다.
-	2. anchor 라는 이름의 토큰을 만듭니다. 명령어를 사용했을 때 이 토큰이 있는 페이지를 기준으로 원형토큰을 생성하게 됩니다.
-	3. 사용할 원형들의 샘플 토큰들을 하나씩 등록합니다. 타입명(정령,기사,소녀 등등)을 이름으로, 원형의 아이콘 이미지를 토큰 이미지로 등록해 놓습니다.
-	   이 샘플의 타입명과 이미지를 바탕으로 실제 원형토큰을 생성하게 됩니다.
-	   (2~3의 토큰들은 실제 게임에 사용하는 것이 아니라 코드에서 사용하기 위한 것입니다. 눈에 안 띄게 구석에 몰아 놓으셔도 좋습니다.)
-	4. 세션방의 대문에 해당하는 페이지에서 [설정]->[API 스크립트]를 선택해 스크립트 수정 페이지로 들어갑니다. (PRO 계정에서만 이 메뉴가 보입니다.)
-	5. New Script에 이 코드들을 복사해 붙여놓고 [Save Script]로 저장합니다. 
-	6. 페이지 아래쪽의 API Output Console에 에러 메시지가 표시되지 않는다면 정상적으로 적용된 것입니다. 세션방에서 테스트를 진행할 수 있습니다.
+	2. 세션방의 대문에 해당하는 페이지에서 [설정]->[API 스크립트]를 선택해 스크립트 수정 페이지로 들어갑니다. (PRO 계정에서만 이 메뉴가 보입니다.)
+	3. New Script에 이 코드들을 복사해 붙여놓고 [Save Script]로 저장합니다.
+	4. 페이지 아래쪽의 API Output Console에 에러 메시지가 표시되지 않는다면 정상적으로 적용된 것입니다. 세션방에서 테스트를 진행할 수 있습니다.
+	
+	5. 세션방에서 소환에 사용할 원형들을 Card 덱으로 만듭니다.
+	   카드들의 이름은 '정령','마검'등 원형의 이름으로 설정하고 덱의 이름은 'archetype'으로 지정합니다.
+	   이제 스크립트는 archetype 덱을 원형 타입의 저장소로 인식하고 여기에서 아이콘을 불러옵니다.
+	6. 원형이 소환될 페이지의 이름을 'spellbound'로 지정합니다.
+	   이제 스크립트는 spellbound 페이지를 마법전을 진행하는 페이지로 인식하고 원형을 생성합니다.
 	7. 채팅창에 '!소환 <특기명>의 <원형타입> <블록숫자>'의 형식으로 입력해 테스트를 해봅니다. (예: !소환 정적의 정령 2 )
 
 	[ 옵션 ]
@@ -31,7 +33,7 @@ on("chat:message", function(msg)
 {
 if (msg.type == "api"){
     if (msg.content.indexOf("!소환") === 0) { //명령어는 변경하셔도 됩니다. 시작은 무조건 느낌표.
-	    var anchor = findObjs({ _type: "graphic", name: "anchor"},{caseInsensitive: true})[0];
+	    //var anchor = findObjs({ _type: "graphic", name: "anchor"},{caseInsensitive: true})[0];
 	    var section = msg.content.split(" ");
 	    var skill = section[1];
 	    var type = section[2];
@@ -39,9 +41,10 @@ if (msg.type == "api"){
 	    if (section.length > 3){
 	        block = section[3];
 	    }
-	    var archetypes = findObjs({ _type: "graphic", name: type},{caseInsensitive: true});
-	    if (archetypes.length > 0) {
-	        var characterImage = archetypes[0].get('imgsrc');
+	    var archetype_deck = findObjs({ _type: 'deck', name: 'archetype'})[0];
+	    var archetype = findObjs({ _type: "card", _deckid: archetype_deck.get('_id'), name:type});
+	    if (archetype.length > 0) {
+	        var characterImage = archetype[0].get('avatar');
 	        characterImage = characterImage.replace("med","thumb");
 	        characterImage = characterImage.replace("max","thumb");
             createObj("graphic", 
@@ -57,12 +60,12 @@ if (msg.type == "api"){
 								controlledby:"all",
 								bar2_value: block?parseInt(block):null,
 								bar2_max: block?parseInt(block):null,
-								pageid: anchor.get("pageid"),
+								pageid: findObjs({ _type: 'page', name: 'spellbound'})[0].get('_id'),
 								imgsrc: characterImage,
 								layer: "objects"
 							});
 	    } else {
-	        sendChat("system","원형 타입 <"+type+">가 존재하지 않습니다.");
+	        sendChat("system","/w gm 원형 타입 <"+type+">가 존재하지 않습니다.");
 	    }
 	}
 }
