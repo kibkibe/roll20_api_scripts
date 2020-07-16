042745934757f1971125d28a58c5cb5e6299774c
yangsalt <kibkibe@naver.com>
2020-07-16 13:06:26 +0900

    Update magicalogia_install_magic.js

--- magicalogia_install_magic.js
+++ magicalogia_install_magic.js
@@ -1,10 +1,38 @@
+/*
+	* by 양천일염
+	* https://github.com/kibkibe/roll20_api_scripts
+	* 200716
+	
+	[ 소개 ]
+    
+	마기카로기아 Roll20 캐릭터 시트의 장서 목록에 이름, 타입, 코스트, 효과, 주구 등의 항목을 채팅창 입력 한번으로 손쉽게 입력하는 기능입니다.
+	
+	[ 사용법 ]
+	1. 세션방의 대문에 해당하는 페이지에서 [설정]->[API 스크립트]를 선택해 스크립트 수정 페이지로 들어갑니다. (PRO 계정에서만 이 메뉴가 보입니다.)
+	2. New Script에 이 코드들을 복사해 붙여놓고 [Save Script]로 저장합니다. 
+	3. 페이지 아래쪽의 API Output Console에 에러 메시지가 표시되지 않는다면 정상적으로 적용된 것입니다. 세션방에서 테스트를 진행할 수 있습니다.
+	4. 채팅창에 아래의 형식으로 입력해 테스트를 해봅니다.
+	
+	!장서 {{
+	--이름1 --타입 --지정특기 --효과 --주구 --이미지주소
+	--이름2 --타입 --지정특기 --효과 --주구 --이미지주소
+	--이름3 --타입 --지정특기 --효과 --주구 --이미지주소
+	}}
+	
+	[ 주의사항 ]
+	'{{'과 '}}' 사이의 내용에 대괄호({})가 포함되면 에러가 발생합니다.
+	본문 내에는 다른 형식의 괄호를 사용해주세요.
+	
+	[ 옵션 ]
+	- 원하신다면 스크립트 내의 주석을 참고해 명령어를 변경하실 수 있습니다.
+	- 채팅창에 명령어를 모두 입력하는 것이 불편하다면 매크로를 이용하시는 것도 추천합니다.
+*/
 on("chat:message", function(msg)
 {
 if (msg.type == "api"){
-if (msg.content.indexOf("!install_magic ") === 0) {
+if (msg.content.indexOf("!장서 ") === 0) {
 	    if (msg.selected) {
 	        var tok = getObj("graphic", msg.selected[0]._id);
-	        log(msg.content);
     	    if (tok) {
     	        var proc_msg = msg.content.replace("!install_magic ").replace("{{").replace("}}");
     	        var cha_id = getObj("character", tok.get('represents')).get('_id');
@@ -61,5 +89,6 @@ if (msg.content.indexOf("!install_magic ") === 0) {
     	        }
     	    }
 	    }
-	}
+    }
+    }
 });
