3ec8316599727fa1f4298ca9a108e40ea2d16c61
yangsalt <kibkibe@naver.com>
2020-07-16 13:47:48 +0900

    Update magicalogia_install_magic.js

--- magicalogia_install_magic.js
+++ magicalogia_install_magic.js
@@ -11,21 +11,21 @@
 	1. 세션방의 대문에 해당하는 페이지에서 [설정]->[API 스크립트]를 선택해 스크립트 수정 페이지로 들어갑니다. (PRO 계정에서만 이 메뉴가 보입니다.)
 	2. New Script에 이 코드들을 복사해 붙여놓고 [Save Script]로 저장합니다. 
 	3. 페이지 아래쪽의 API Output Console에 에러 메시지가 표시되지 않는다면 정상적으로 적용된 것입니다. 세션방에서 테스트를 진행할 수 있습니다.
-	4. 채팅창에 아래의 형식으로 입력해 테스트를 해봅니다.
+	4. 장서를 입력할 캐릭터의 토큰을 클릭해 선택한 상태로 채팅창에 아래의 형식으로 입력해 테스트를 해봅니다.
 	
 	!장서 {{
-	--이름1 --타입 --지정특기 --효과 --주구 --이미지주소
-	--이름2 --타입 --지정특기 --효과 --주구 --이미지주소
-	--이름3 --타입 --지정특기 --효과 --주구 --이미지주소
+	이름1 --타입 --지정특기 --코스트 --효과 --주구 --이미지주소
+	이름2 --타입 --지정특기 --코스트 --효과 --주구 --이미지주소
+	이름3 --타입 --지정특기 --코스트 --효과 --주구 --이미지주소
 	}}
 	
 	[ 주의사항 ]
-	'{{'과 '}}' 사이의 내용에 대괄호({})가 포함되면 에러가 발생합니다.
-	본문 내에는 다른 형식의 괄호를 사용해주세요.
+	- '{{'과 '}}' 사이의 내용에 대괄호({})가 포함되면 에러가 발생합니다. 본문 내에는 다른 형식의 괄호를 사용해주세요.
+	- 지정특기나 코스트가 없는 마법의 경우 -- 뒤를 생략하시면 됩니다. (예: --타입 -- -- --효과)
+	- 캐릭터 토큰을 선택하지 않거나 캐릭터와 연결되어 있지 않은 토큰을 선택한 채로는 장서를 입력할 수 없습니다.
 	
 	[ 옵션 ]
 	- 원하신다면 스크립트 내의 주석을 참고해 명령어를 변경하실 수 있습니다.
-	- 채팅창에 명령어를 모두 입력하는 것이 불편하다면 매크로를 이용하시는 것도 추천합니다.
 */
 on("chat:message", function(msg)
 {
@@ -33,7 +33,8 @@ if (msg.type == "api"){
 if (msg.content.indexOf("!장서 ") === 0) {
 	    if (msg.selected) {
 	        var tok = getObj("graphic", msg.selected[0]._id);
-    	    if (tok) {
+    	    if (tok && tok.get('represents')) {
+    	        try {
     	        var proc_msg = msg.content.replace("!install_magic ").replace("{{").replace("}}");
     	        var cha_id = getObj("character", tok.get('represents')).get('_id');
     	        var list = proc_msg.split("<br/>\n");
@@ -56,24 +57,36 @@ if (msg.content.indexOf("!장서 ") === 0) {
     	            }
     	        }
     	        
+    	        var find_key = function(str){
+    	            var attr_from_value = findObjs({_type: "attribute", _characterid: cha_id, current: str});
+    	            for (var i=0;i<attr_from_value.length;i++) {
+    	                var attr = attr_from_value[i].get('name');
+    	                if (!attr.includes('Magic')) {
+    	                    return attr;
+    	                }
+    	            }
+    	        }
+    	        
     	        for (var i=0;i<list.length;i++) {
         	        var items = list[i].split(" --");
         	        if (items.length > 1) {
         	            idx++;
         	            for (var j=0;j<items.length;j++) {
         	                var name, attr, item;
-        	                if (idx < 7) {
-        	                    name = attr_list[j].replace("_*num*", ("_0"+idx).slice(-3));
+        	                if (idx < 11) {
+        	                    name = attr_list[j].replace("_*num*", "_"+ ("0"+idx).slice(-2));
         	                    attr = findObjs({_type: "attribute", _characterid: cha_id, name:name})[0];
         	                } else {
-        	                    name = repeat_id_list[idx-7] + "_" + attr_list[j].replace("_*num*", "");
-    	                        log(name);
+        	                    name = repeat_id_list[idx-11] + "_" + attr_list[j].replace("_*num*", "");
         	                    attr = findObjs({_type: "attribute", _characterid: cha_id, name:name})[0];
     	        
         	                }
         	                item = items[j];
         	                if (j==1||j==2) {
-        	                    item = "@{" + item + "}";
+        	                    var attr_key = find_key(item);
+        	                    if (attr_key) {
+        	                        item = "@{" + attr_key + "}";
+        	                    }
         	                }
         	                
         	                if (!attr) {
@@ -87,8 +100,11 @@ if (msg.content.indexOf("!장서 ") === 0) {
         	            }
         	        }
     	        }
+    	        }  catch(err) {
+                    sendChat("장서", "/w gm Error occured: "+err);
+                }
     	    }
-	    }
+        }
     }
-    }
+}
 });
