adad878d5e2675698c20e17491a5d0f6f47d0e5c
yangsalt <kibkibe@naver.com>
2020-12-15 22:35:56 +0900

    Update as_autofiller.js

--- as_autofiller.js
+++ as_autofiller.js
@@ -1,5 +1,5 @@
 /* 설치법: https://github.com/kibkibe/roll20_api_scripts/wiki/%5B%EC%84%A4%EC%B9%98%EB%B2%95%5D-as_autofiller.js */
-/* (as_autofiller.js) 201213 코드 시작 */
+/* (as_autofiller.js) 201215 코드 시작 */
 on("chat:message", function(msg)
 {
 if (msg.type == "api"){
